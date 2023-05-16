503850c6478bcfbd32250ab7e07798e5cb4b9348
Shri10 <pethe.shriraj@gmail.com>
2023-05-16 07:56:35 +0530

    [Shri10] first commit

---
+++ Day5Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day5Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
